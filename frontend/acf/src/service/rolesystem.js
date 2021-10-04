557097f604093850b5ab367f2cbff3955eef5f90
tranminh99 <68344766+tranminh99@users.noreply.github.com>
2021-10-04 09:55:03 +0700

    update class rolesystem.js by new structure axios

--- frontend/acf/src/service/rolesystem.js
+++ frontend/acf/src/service/rolesystem.js
@@ -1,17 +1,12 @@
-import axios from 'axios';
-import authHeader from './auth-header';
-
-const API_URL = 'http://localhost:8080/api/';
+import http from "./http-common";
 
 class RoleService {
-    getAllAccount(user) {
-        return axios.post(API_URL + 'spadmin/account/allaccount', user, { headers: authHeader() });
-    }
-
-    searchAccount(name){
-        return axios.post(API_URL + 'spadmin/account/searchaccount', name, { headers: authHeader() });
-    }
-
+  getAllAccount(user) {
+    return http.post(`/spadmin/account/allaccount`, user);
+  }
+  searchAccount(name) {
+    return http.post("/spadmin/account/searchaccount", name);
+  }
 }
 
 export default new RoleService();
