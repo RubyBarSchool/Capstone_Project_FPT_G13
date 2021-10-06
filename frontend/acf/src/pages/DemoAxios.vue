caf63135611b5673ad950ab9f1c40ba327403ec7
tranvutruong <truongtv1399it@gmail.com>
2021-10-06 17:39:00 +0700

    update code

--- frontend/acf/src/pages/DemoAxios.vue
+++ frontend/acf/src/pages/DemoAxios.vue
@@ -1,38 +1,83 @@
 <template>
-  <div>xin chao man profile</div>
+  <div>
+    <a-table
+    :columns="columns" 
+      :data-source="dataSourceTable"
+      :rowKey="
+        (record, index) => {
+          return index;
+        }
+      "
+    >
+      <template slot="name" slot-scope="text, record">
+        {{record.id}}{{record.name}}
+      </template>
+      <template slot="action" slot-scope="text, record">
+        <a-popconfirm
+          v-if="dataSourceTable.length"
+          title="Sure to delete?"
+          @confirm="() => onDelete(record.id)"
+        >
+          <a href="javascript:;">Delete</a>
+        </a-popconfirm>
+      </template>
+    </a-table>
+  </div>
 </template>
-
  <script>
-// import DemoAxios from "../service/demoaxios";
-// export default {
-//   name: "Profile",
-//   data() {
-//     return {};
-//   },
-//   computed: {
-//     loggedIn() {
-//       return this.$store.state.auth.status.loggedIn;
-//     },
-//   },
-//   created() {
-//     this.retrieveTutorials();
-//   },
-//   methods: {
-//     retrieveTutorials() {
-//       let form = {
-//         pageIndex: 0,
-//         pageSize: 10,
-//       };
-//       DemoAxios.getAll(form)
-//         .then((response) => {
-//           console.log(response.data);
-//         })
-//         .catch((e) => {
-//           console.log(e);
-//         });
-//     },
-//   },
-// };
+import DemoAxios from "../service/demoaxios";
+export default {
+  name: "DemoAxios",
+  data() {
+    return {
+      dataSourceTable: [],
+      columns: [
+        {
+          title: "ID",
+          dataIndex: "id",
+          key: "id",
+        },
+        {
+          title: "name",
+          dataIndex: "name",
+          key: "name",
+          width: "30%",
+          scopedSlots: { customRender: "name" },
+        },
+        {
+          title: "Action",
+          dataIndex: "action",
+          key: "action",
+          scopedSlots: { customRender: "action" },
+        },
+      ],
+    };
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.status.loggedIn;
+    },
+  },
+  created() {
+    this.retrieveTutorials();
+  },
+  methods: {
+    retrieveTutorials() {
+      let form = {
+        name: "",
+        pageIndex: 1,
+        pageSize: 10,
+      };
+      DemoAxios.getAll(form)
+        .then((response) => {
+          this.dataSourceTable = response.data.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
 </script>
 
 <style>
