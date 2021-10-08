c5eaae2b08420ff0147cff43129b003596a47b5a
AnhLHHE130300 <anhlhhe130300@fpt.edu.vn>
2021-10-08 23:30:53 +0700

    udpate

--- frontend/acf/src/router.js
+++ frontend/acf/src/router.js
@@ -24,7 +24,7 @@ export const router = new Router({
             name: 'admin',
             // lazy-loaded
             component: () =>
-                import ('./pages/V3.vue')
+                import ('./pages/Admin.vue')
         },
         {
             path: '/demoaxios',
