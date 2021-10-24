0449625c75ae23570de65a36500f58c57c519384
tranvutruong <truongtv1399it@gmail.com>
2021-10-24 17:34:07 +0700

    update attendance

--- frontend/acf/src/pages/Attendance.vue
+++ frontend/acf/src/pages/Attendance.vue
@@ -148,6 +148,7 @@ export default {
       this.reloadSelect();
       this.typeAttendance = "1";
       this.getDate();
+      this.onChangeDate();
     },
     reloadSelect() {
       this.selectedRowKeys = [];
