a0f6b15dc0ea9c50b408f7d717918a46e09f38a6
AnhLHHE130300 <anhlhhe130300@fpt.edu.vn>
2021-11-02 16:56:52 +0700

    add AcceptXinNghi.vue and XinNghi.vue

---
+++ frontend/acf/src/pages/AcceptXinNghi.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="acceptxinnghi">
+    <a-layout :style="{ background: 'white' }">
+      <Header />
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div
+          :style="{
+            minHeight: '360px',
+            background: 'white',
+          }"
+        >
+          <a-back-top>
+            <div class="ant-back-top-inner">
+              <font-awesome-icon
+                :icon="['fas', 'angle-double-up']"
+                :style="{ width: '160px', height: '50px', color: '#15AABF' }"
+              />
+            </div>
+          </a-back-top>
+          <!-- menu trên -->
+          <a-input
+            placeholder="Tiêu đề"
+            :style="{ 'margin-right': '5px', width: '200px' }"
+          />
+          <a-input
+            placeholder="Nội dung"
+            :style="{ 'margin-right': '5px', width: '200px' }"
+          />
+          <a-range-picker
+            :placeholder="['Ngày bắt đầu', 'Ngày kết thúc']"
+            format="DD/MM/YYYY"
+          />
+          <a-button
+            type="primary"
+            :style="{ 'margin-left': '5px' }"
+            @click="showModalAdd"
+          >
+            <font-awesome-icon
+              :icon="['fas', 'user-edit']"
+              :style="{ 'margin-right': '5px' }"
+            />
+            Xem đơn
+          </a-button>
+          <!-- menu trên -->
+
+          <!-- table content -->
+
+          <!-- table content -->
+
+          <!-- popup view -->
+          <a-modal v-model="visibleView" title="Xét đơn xin nghỉ">
+            <template slot="footer">
+              <a-button key="back" @click="handleCancel"> Hủy </a-button>
+              <a-button type="danger"> Loại bỏ </a-button>
+              <a-button type="primary"> Chấp nhận </a-button>
+            </template>
+            <div class="container">
+              <a-form-model>
+                <a-form-model-item label="Tiêu đề">
+                  <a-input disabled />
+                </a-form-model-item>
+                <a-form-model-item label="Chọn ngày">
+                  <a-date-picker placeholder="Chọn ngày" disabled />
+                </a-form-model-item>
+                <a-form-model-item label="Nội dung">
+                  <a-textarea
+                    placeholder="Viết nội dung....."
+                    :rows="4"
+                    disabled
+                  />
+                </a-form-model-item>
+                <a-form-model-item label="Ghi chú">
+                  <a-textarea placeholder="Viết ghi chú....." :rows="4" />
+                </a-form-model-item>
+              </a-form-model>
+            </div>
+          </a-modal>
+          <!-- popup add -->
+        </div>
+      </a-layout-content>
+      <Footer />
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import Header from "@/layouts/Header.vue";
+import Footer from "@/layouts/Footer.vue";
+export default {
+  name: "AcceptXinNghi",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      visibleView: false,
+    };
+  },
+  methods: {
+    handleCancel() {
+      this.visibleView = false;
+    },
+
+    showModalAdd() {
+      this.visibleView = true;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/acf/src/pages/XinNghi.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="xinnghi">
+    <a-layout :style="{ background: 'white' }">
+      <Header />
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div
+          :style="{
+            minHeight: '360px',
+            background: 'white',
+          }"
+        >
+          <a-back-top>
+            <div class="ant-back-top-inner">
+              <font-awesome-icon
+                :icon="['fas', 'angle-double-up']"
+                :style="{ width: '160px', height: '50px', color: '#15AABF' }"
+              />
+            </div>
+          </a-back-top>
+          <!-- menu trên -->
+          <a-input
+            placeholder="Tiêu đề"
+            :style="{ 'margin-right': '5px', width: '200px' }"
+          />
+          <a-input
+            placeholder="Nội dung"
+            :style="{ 'margin-right': '5px', width: '200px' }"
+          />
+          <a-range-picker
+            :placeholder="['Ngày bắt đầu', 'Ngày kết thúc']"
+            format="DD/MM/YYYY"
+          />
+          <a-button
+            type="primary"
+            :style="{ 'margin-left': '5px' }"
+            @click="showModalAdd"
+          >
+            <font-awesome-icon
+              :icon="['fas', 'user-edit']"
+              :style="{ 'margin-right': '5px' }"
+            />
+            Viết đơn
+          </a-button>
+          <!-- menu trên -->
+
+          <!-- table content -->
+         
+          <!-- table content -->
+
+          <!-- popup add -->
+          <a-modal v-model="visibleAdd" title="Đơn xin nghỉ">
+            <template slot="footer">
+              <a-button key="back" @click="handleCancel"> Hủy </a-button>
+              <a-button key="submit" type="primary"> Lưu </a-button>
+            </template>
+            <div class="container">
+              <a-form-model>
+                <a-form-model-item label="Tiêu đề">
+                  <a-input />
+                </a-form-model-item>
+                <a-form-model-item label="Chọn ngày">
+                  <a-date-picker placeholder="Chọn ngày" />
+                </a-form-model-item>
+                <a-form-model-item label="Nội dung">
+                  <a-textarea placeholder="Viết nội dung" :rows="4" />
+                </a-form-model-item>
+              </a-form-model>
+            </div>
+          </a-modal>
+          <!-- popup add -->
+        </div>
+      </a-layout-content>
+      <Footer />
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import Header from "@/layouts/Header.vue";
+import Footer from "@/layouts/Footer.vue";
+export default {
+  name: "XinNghi",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      visibleAdd: false,
+    };
+  },
+  methods: {
+    handleCancel() {
+      this.visibleAdd = false;
+    },
+
+    showModalAdd() {
+      this.visibleAdd = true;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
