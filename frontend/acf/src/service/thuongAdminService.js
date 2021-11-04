2480ce52a71be885d400bd756108d4639343989c
tranminh99 <68344766+tranminh99@users.noreply.github.com>
2021-11-04 11:46:26 +0700

    update file thuongAdminService.js

---
+++ frontend/acf/src/service/thuongAdminService.js
@@ -0,0 +1,24 @@
+import http from "./http-common";
+
+class thuongAdminService {
+    searchThuongAdmin(form) {
+        return http.post("/admin/bonus/search", form);
+    }
+    getAllEmployee(form) {
+        return http.post("/admin/employee/search", form);
+    }
+    addThuongAdmin(form) {
+        return http.post(`/admin/bonus/add`, form);
+    }
+    // updateThuongAdmin(data) {
+    //     return http.put(`/employee/advanceSalary/update`, data);
+    // }
+    // getAdvanceSalaryEmployeeByID(id) {
+    //     return http.get(`/employee/advanceSalary/detail?id=${id}`);
+    // }
+    deleteThuongAdmin(id) {
+        return http.delete(`/admin/bonus/delete?id=${id}`);
+    }
+}
+
+export default new thuongAdminService();
