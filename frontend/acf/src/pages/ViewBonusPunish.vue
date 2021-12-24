ae7ff94a765e6ba7e6157c57da9c954ba860aa2a
tranminh99 <68344766+tranminh99@users.noreply.github.com>
2021-12-24 18:54:25 +0700

    update code

--- frontend/acf/src/pages/ViewBonusPunish.vue
+++ frontend/acf/src/pages/ViewBonusPunish.vue
@@ -275,12 +275,14 @@ export default {
     },
     submitSearch() {
       this.dataSearch.total = 0;
+      this.dataSearch.pageIndex = 1;
       viewThuongPhat
         .getBonusAndPunish(this.dataSearch)
         .then((response) => {
           this.dataSourceTable = response.data.data;
           this.dataSearch.total = response.data.total;
           this.pagination.total = response.data.total;
+          this.pagination.current = 1;
         })
         .catch((e) => {
           console.log(e);
